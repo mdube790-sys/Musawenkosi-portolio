d7b1e6e69b5967db746202d0aed795243e16fb46
mdube790-sys <mdube790@gmail.com>
2026-06-30 16:23:48 +0200

    Add missing config files and GitHub Actions workflow

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Services from './components/Services'
+import CaseStudies from './components/CaseStudies'
+import Projects from './components/Projects'
+import Achievements from './components/Achievements'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Services />
+      <CaseStudies />
+      <Projects />
+      <Achievements />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
